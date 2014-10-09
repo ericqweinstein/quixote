dbd9cd3794038fd4f1fb48fdf5be96bfd8446d52
Eric Weinstein <eric.q.weinstein@gmail.com>
2014-10-09 09:05:29 -0400

    Set up New Relic monitoring

--- project.clj
+++ project.clj
@@ -23,4 +23,5 @@
              :uberjar {:aot :all}}
   :aliases {"lint" ^{:doc "Lint and test all the things"}
             ["do" "ancient," "kibit," "bikeshed" "-m135"]}
-  :min-lein-version "2.0.0")
+  :min-lein-version "2.0.0"
+  :java-agents [[com.newrelic.agent.java/newrelic-agent "3.11.0"]])
