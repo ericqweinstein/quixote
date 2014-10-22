47f978c8e72e808fa5c9296aa05d76f701a048a4
Eric Weinstein <eric.q.weinstein@gmail.com>
2014-10-21 20:44:28 -0400

    Increase search hang time

--- resources/javascripts/cityshelf.js
+++ resources/javascripts/cityshelf.js
@@ -121,7 +121,7 @@ CityShelf.controller('MainCtrl', ['$scope', '$location', '$route', 'Search', 'Ge
 
     if (Geolocation.fetch().length) {
       // See above comment re: hack. (EW 30 Sep 2014)
-      sleep(1000, function() {
+      sleep(4000, function() {
         $location.path('/search');
         $route.reload();
       });
