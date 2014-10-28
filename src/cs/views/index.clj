af5be271fa941ab19aba65bc2f64ad13b0cb274f
Eric Weinstein <eric.q.weinstein@gmail.com>
2014-10-27 21:29:46 -0400

    Set up Google Analytics

--- src/cs/views/index.clj
+++ src/cs/views/index.clj
@@ -2,7 +2,8 @@
 ; @author Eric Weinstein <eric.q.weinstein@gmail.com>
 
 (ns cs.views.index
-  (:use [hiccup.page :only (html5 include-css include-js)]))
+  (:use [hiccup.page :only (html5 include-css include-js)]
+        [hiccup.element :only (javascript-tag)]))
 
 (defn index
   "Provides the base CityShelf view."
@@ -34,4 +35,12 @@
           (include-js "javascripts/vendor/angular/angular-route.min.js")
           (include-js "javascripts/vendor/lodash.underscore.min.js")
           (include-js "javascripts/vendor/angular/angular-google-maps.min.js")
-          (include-js "javascripts/cityshelf.min.js")]))
+          (include-js "javascripts/cityshelf.min.js")
+          (javascript-tag
+            "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+             (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+             m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+             })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+             ga('create', 'UA-56171518-1', 'auto');
+             ga('send', 'pageview');")]))
