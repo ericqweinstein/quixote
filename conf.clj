fc57f8db5769a03d0573b9ff84f94b8b112079eb
Eric Weinstein <eric.q.weinstein@gmail.com>
2015-05-15 19:15:35 -0400

    Add city annotations to conf.clj

--- conf.clj
+++ conf.clj
@@ -1,39 +1,47 @@
 [{:id 0
+  :city :nyc
   :map {:center {:latitude 40.763754 :longitude -73.923849} :zoom 14}
   :phone "tel:+1-718-278-2665"
   :storeLink "http://www.astoriabookshop.com"
   :storeName "Astoria Bookshop"}
  {:id 1
+  :city :nyc
   :map {:center {:latitude 40.805786 :longitude -73.966143} :zoom 14}
   :phone "tel:+1-212-678-1654"
   :storeLink "http://www.bankstreetbooks.com"
   :storeName "Bank Street Books"}
  {:id 2
+  :city :nyc
   :map {:center {:latitude 40.805135 :longitude -73.964991} :zoom 14}
   :phone "tel:+1-212-865-1588"
   :storeLink "http://www.bookculture.com"
   :storeName "Book Culture"}
  {:id 3
+  :city :nyc
   :map {:center {:latitude 40.672900 :longitude -73.976457} :zoom 14}
   :phone "tel:+1-718-783-3075"
   :storeLink "http://site.booksite.com/6994"
   :storeName "Community Bookstore"}
  {:id 4
+  :city :nyc
   :map {:center {:latitude 40.686502 :longitude -73.974571} :zoom 14}
   :phone "tel:+1-718-246-0200"
   :storeLink "http://www.greenlightbookstore.com"
   :storeName "Greenlight Bookstore"}
  {:id 5
+  :city :nyc
   :map {:center {:latitude 40.723518 :longitude -73.996061} :zoom 14}
   :phone "tel:+1-212-274-1160"
   :storeLink "http://www.mcnallyjackson.com"
   :storeName "McNally Jackson"}
  {:id 6
+  :city :nyc
   :map {:center {:latitude 40.729921 :longitude -73.989448} :zoom 14}
   :phone "tel:+1-212-260-7853"
   :storeLink "http://www.stmarksbookshop.com"
   :storeName "St. Mark's Bookshop"}
  {:id 7
+  :city :nyc
   :map {:center {:latitude 40.729197 :longitude -73.957319} :zoom 14}
   :phone "tel:+1-718-383-0096"
   :storeLink "http://www.wordbookstores.com"
