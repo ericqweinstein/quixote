5eac6479a1daa1df204f1e95bc059a072ebc0a4d
Eric Weinstein <eric.q.weinstein@gmail.com>
2015-05-18 20:16:19 -0400

    Add CHI and SEA test cities

--- conf.clj
+++ conf.clj
@@ -1,48 +1,77 @@
 [{:id 0
   :city :nyc
+  :type :site
   :map {:center {:latitude 40.763754 :longitude -73.923849} :zoom 14}
   :phone "tel:+1-718-278-2665"
   :storeLink "http://www.astoriabookshop.com"
   :storeName "Astoria Bookshop"}
  {:id 1
   :city :nyc
+  :type :site
   :map {:center {:latitude 40.805786 :longitude -73.966143} :zoom 14}
   :phone "tel:+1-212-678-1654"
   :storeLink "http://www.bankstreetbooks.com"
   :storeName "Bank Street Books"}
  {:id 2
   :city :nyc
+  :type :site
   :map {:center {:latitude 40.805135 :longitude -73.964991} :zoom 14}
   :phone "tel:+1-212-865-1588"
   :storeLink "http://www.bookculture.com"
   :storeName "Book Culture"}
  {:id 3
   :city :nyc
+  :type :booksite
   :map {:center {:latitude 40.672900 :longitude -73.976457} :zoom 14}
   :phone "tel:+1-718-783-3075"
   :storeLink "http://site.booksite.com/6994"
   :storeName "Community Bookstore"}
  {:id 4
   :city :nyc
+  :type :site
   :map {:center {:latitude 40.686502 :longitude -73.974571} :zoom 14}
   :phone "tel:+1-718-246-0200"
   :storeLink "http://www.greenlightbookstore.com"
   :storeName "Greenlight Bookstore"}
  {:id 5
   :city :nyc
+  :type :solr
   :map {:center {:latitude 40.723518 :longitude -73.996061} :zoom 14}
   :phone "tel:+1-212-274-1160"
   :storeLink "http://www.mcnallyjackson.com"
   :storeName "McNally Jackson"}
  {:id 6
   :city :nyc
+  :type :site
   :map {:center {:latitude 40.729921 :longitude -73.989448} :zoom 14}
   :phone "tel:+1-212-260-7853"
   :storeLink "http://www.stmarksbookshop.com"
   :storeName "St. Mark's Bookshop"}
  {:id 7
   :city :nyc
+  :type :site
   :map {:center {:latitude 40.729197 :longitude -73.957319} :zoom 14}
   :phone "tel:+1-718-383-0096"
   :storeLink "http://www.wordbookstores.com"
-  :storeName "Word Bookstore"}]
+  :storeName "Word Bookstore"}
+ {:id 8
+  :city :chi
+  :type :site
+  :map {:center {:latitude 41.967562 :longitude -87.687986} :zoom 14}
+  :phone "tel+1-773-293-2665"
+  :storeLink "http://www.bookcellarinc.com"
+  :storeName "The Book Cellar"}
+ {:id 9
+  :city :chi
+  :type :solr
+  :map {:center {:latitude 41.926976 :longitude -87.707253} :zoom 14}
+  :phone "tel+1-773-235-2523"
+  :storeLink "http://www.citylitbooks.com"
+  :storeName "City Lit Books"}
+ {:id 10
+  :city :sea
+  :type :site
+  :map {:center {:latitude 47.614661 :longitude -122.319602} :zoom 14}
+  :phone "tel+1-206-624-6600"
+  :storeLink "http://www.elliottbaybook.com"
+  :storeName "The Elliott Bay Book Company"}]
