c2bb4a1caa410a9f66a543747784acf3f5788d23
Eric Weinstein <eric.q.weinstein@gmail.com>
2015-05-24 12:51:15 -0400

    Fix test

--- test/cs/utils_test.clj
+++ test/cs/utils_test.clj
@@ -6,8 +6,8 @@
             [cs.utils :refer :all]))
 
 (def fixture-data
-  [
-   {:isbn "978000000001"
+  [(sorted-map
+    :isbn "978000000001"
     :search-result {
                     :title "Book A"
                     :author "Author A"
@@ -16,89 +16,77 @@
                     :available true
                     :price 11.99
                    }
-    }
-   {:isbn "978000000002"
-    :search-result {
-                    :title "Book B"
-                    :author "Author B"
-                    :img "http://www.example.com/B"
-                    :store 0
-                    :available false
-                    :price 11.99
+    )
+   (sorted-map
+     :isbn "978000000002"
+     :search-result {
+                     :title "Book B"
+                     :author "Author B"
+                     :img "http://www.example.com/B"
+                     :store 0
+                     :available false
+                     :price 11.99
+                    }
+    )
+   (sorted-map
+     :isbn "978000000003"
+     :search-result {
+                     :title "Book C"
+                     :author "Author C"
+                     :img "http://www.example.com/C"
+                     :store 0
+                     :available true
+                     :price 11.99
                    }
-    }
-   {:isbn "978000000003"
-    :search-result {
-                    :title "Book C"
-                    :author "Author C"
-                    :img "http://www.example.com/C"
-                    :store 0
-                    :available true
-                    :price 11.99
-                   }
-    }
-   {:isbn "978000000001"
-    :search-result {
-                    :title "Book A"
-                    :author "Author A"
-                    :img "http://www.example.com/A"
-                    :store 1
-                    :available false
-                    :price 10.99
-                   }
-    }
-   {:isbn "978000000002"
-    :search-result {
-                    :title "Book B"
-                    :author "Author B"
-                    :img "http://www.example.com/B"
-                    :store 1
-                    :available true
-                    :price 12.99
-                   }
-    }
+    )
+   (sorted-map
+     :isbn "978000000001"
+     :search-result {
+                     :title "Book A"
+                     :author "Author A"
+                     :img "http://www.example.com/A"
+                     :store 1
+                     :available false
+                     :price 10.99
+                    }
+   )
+   (sorted-map
+     :isbn "978000000002"
+     :search-result {
+                     :title "Book B"
+                     :author "Author B"
+                     :img "http://www.example.com/B"
+                     :store 1
+                     :available true
+                     :price 12.99
+                    }
+    )
   ]
 )
 
 (facts "About manipulating book data"
        (fact "It converts data from search results
              to books with search result fields")
-       ;; TODO: Figure out why Midje reverses the data structure here.
-       ;; The keys/values are swapped in the test, but are delivered
-       ;; correctly by the API. (EW 23 May 2015)
-       (pivot fixture-data) => {
-                                {:store 1
-                                 :title "Book A"
-                                 :author "Author A"
-                                 :available false
-                                 :price 10.99
-                                 :img "http://www.example.com/A"}
-                                ["978000000001"]
-                                {:store 0
-                                 :title "Book B"
-                                 :author "Author B"
-                                 :available false
-                                 :price 11.99
-                                 :img "http://www.example.com/B"}
-                                ["978000000002"]
-                                {:store 0
-                                 :title "Book C"
-                                 :author "Author C"
-                                 :available true
-                                 :price 11.99
-                                 :img "http://www.example.com/C"}
-                                ["978000000003"]
-                                {:store 0
-                                 :title "Book A"
-                                 :author "Author A"
-                                 :available true
-                                 :price 11.99
-                                 :img "http://www.example.com/A"}
-                                ["978000000001"]
-                                {:store 1
-                                 :title "Book B"
-                                 :author "Author B"
-                                 :available true
-                                 :price 12.99
-                                 :img "http://www.example.com/B"}
-                                ["978000000002"]})
+       (pivot fixture-data) => {"978000000001"
+                                [{:author "Author A"
+                                  :available true
+                                  :img "http://www.example.com/A"
+                                  :price 11.99 :store 0 :title "Book A"}
+                                 {:author "Author A"
+                                  :available false
+                                  :img "http://www.example.com/A"
+                                  :price 10.99 :store 1 :title "Book A"}]
+                                "978000000002"
+                                [{:author "Author B"
+                                  :available false
+                                  :img "http://www.example.com/B"
+                                  :price 11.99 :store 0 :title "Book B"}
+                                 {:author "Author B"
+                                  :available true
+                                  :img "http://www.example.com/B"
+                                  :price 12.99 :store 1 :title "Book B"}]
+                                "978000000003"
+                                [{:author "Author C"
+                                  :available true
+                                  :img "http://www.example.com/C"
+                                  :price 11.99 :store 0 :title "Book C"}]})
