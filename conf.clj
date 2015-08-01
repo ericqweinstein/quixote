22ee4c977e9ff211a740c7c0f16df3a76012c07c
Eric Weinstein <eric.q.weinstein@gmail.com>
2015-07-31 20:39:33 -0400

    Add store addresses

--- conf.clj
+++ conf.clj
@@ -3,6 +3,7 @@
   :type :site
   :map {:center {:latitude 40.763754 :longitude -73.923849} :zoom 14}
   :phone "tel:+1-718-278-2665"
+  :address "31-29 31st St, New York, NY 11106"
   :storeLink "http://www.astoriabookshop.com"
   :storeName "Astoria Bookshop"}
  {:id 1
@@ -10,6 +11,7 @@
   :type :site
   :map {:center {:latitude 40.805786 :longitude -73.966143} :zoom 14}
   :phone "tel:+1-212-678-1654"
+  :address "2780 Broadway, New York, NY 10025"
   :storeLink "http://www.bankstreetbooks.com"
   :storeName "Bank Street Books"}
  {:id 2
@@ -17,6 +19,7 @@
   :type :site
   :map {:center {:latitude 40.805135 :longitude -73.964991} :zoom 14}
   :phone "tel:+1-212-865-1588"
+  :address "536 W 112th St, New York, NY 10025"
   :storeLink "http://www.bookculture.com"
   :storeName "Book Culture"}
  {:id 3
@@ -24,6 +27,7 @@
   :type :booksite
   :map {:center {:latitude 40.672900 :longitude -73.976457} :zoom 14}
   :phone "tel:+1-718-783-3075"
+  :address "143 7th Ave, Brooklyn, NY 11215"
   :storeLink "http://site.booksite.com/6994"
   :storeName "Community Bookstore"}
  {:id 4
@@ -31,6 +35,7 @@
   :type :site
   :map {:center {:latitude 40.686502 :longitude -73.974571} :zoom 14}
   :phone "tel:+1-718-246-0200"
+  :address "686 Fulton St, Brooklyn, NY 11217"
   :storeLink "http://www.greenlightbookstore.com"
   :storeName "Greenlight Bookstore"}
  {:id 5
@@ -38,6 +43,7 @@
   :type :solr
   :map {:center {:latitude 40.723518 :longitude -73.996061} :zoom 14}
   :phone "tel:+1-212-274-1160"
+  :address "52 Prince St, New York, NY 10012"
   :storeLink "http://www.mcnallyjackson.com"
   :storeName "McNally Jackson"}
  {:id 6
@@ -45,6 +51,7 @@
   :type :site
   :map {:center {:latitude 40.724005 :longitude -73.985879} :zoom 14}
   :phone "tel:+1-212-260-7853"
+  :address "136 E 3rd St, New York, NY 10009"
   :storeLink "http://www.stmarksbookshop.com"
   :storeName "St. Mark's Bookshop"}
  {:id 7
@@ -52,6 +59,7 @@
   :type :site
   :map {:center {:latitude 40.729197 :longitude -73.957319} :zoom 14}
   :phone "tel:+1-718-383-0096"
+  :address "126 Franklin St, Brooklyn, NY 11222"
   :storeLink "http://www.wordbookstores.com"
   :storeName "Word Bookstore"}
  {:id 8
@@ -59,6 +67,7 @@
   :type :site
   :map {:center {:latitude 42.342695 :longitude -71.121423} :zoom 14}
   :phone "tel:+1-617-566-6660"
+  :address "279 Harvard St, Brookline, MA 02446"
   :storeLink "http://www.brooklinebooksmith-shop.com"
   :storeName "Brookline Booksmith"}
  {:id 9
@@ -66,6 +75,7 @@
   :type :site
   :map {:center {:latitude 42.314296 :longitude -71.210320} :zoom 14}
   :phone "tel:+1-617-964-7440"
+  :address "82 Needham St, Newton, MA 02461"
   :storeLink "http://www.nebookfair.com"
   :storeName "New England Mobile Book Fair"}
  {:id 10
@@ -73,6 +83,7 @@
   :type :solr
   :map {:center {:latitude 42.366121 :longitude -71.105278} :zoom 14}
   :phone "tel:+1-617-547-3721"
+  :address "4 Pleasant St, Cambridge, MA 02139"
   :storeLink "http://www.pandemoniumbooks.com"
   :storeName "Pandemonium Books and Games"}
  {:id 11
@@ -80,6 +91,7 @@
   :type :site
   :map {:center {:latitude 42.389177 :longitude -71.118252} :zoom 14}
   :phone "tel:+1-617-491-2220"
+  :address "25 White St, Cambridge, MA 02140"
   :storeLink "http://www.portersquarebooks.com"
   :storeName "Porter Square Books"}
  {:id 12
@@ -87,6 +99,7 @@
   :type :site
   :map {:center {:latitude 41.967562 :longitude -87.687986} :zoom 14}
   :phone "tel:+1-773-293-2665"
+  :address "4736 N Lincoln Ave, Chicago, IL 60625"
   :storeLink "http://www.bookcellarinc.com"
   :storeName "The Book Cellar"}
  {:id 13
@@ -94,6 +107,7 @@
   :type :solr
   :map {:center {:latitude 41.926976 :longitude -87.707253} :zoom 14}
   :phone "tel:+1-773-235-2523"
+  :address "2523 N Kedzie Blvd, Chicago, IL 60647"
   :storeLink "http://www.citylitbooks.com"
   :storeName "City Lit Books"}
  {:id 14
@@ -101,6 +115,7 @@
   :type :site
   :map {:center {:latitude 41.790024 :longitude -87.595605} :zoom 14}
   :phone "tel:+1-773-752-4381"
+  :address "5751 S Woodlawn Ave, Chicago, IL 60637"
   :storeLink "http://www.semcoop.com"
   :storeName "Seminary Co-op Bookstores"}
  {:id 15
@@ -108,6 +123,7 @@
   :type :solr
   :map {:center {:latitude 41.941585 :longitude -87.643732} :zoom 14}
   :phone "tel:+1-773-883-9119"
+  :address "3251 N Broadway St, Chicago, IL 60657"
   :storeLink "http://www.unabridgedbookstore.com"
   :storeName "Unabridged Bookstore"}
  {:id 16
@@ -115,6 +131,7 @@
   :type :solr
   :map {:center {:latitude 41.977334 :longitude -87.667873} :zoom 14}
   :phone "tel:+1-773-769-9299"
+  :address "5233 N Clark St, Chicago, IL 60640"
   :storeLink "http://www.womenandchildrenfirst.com"
   :storeName "Women and Children First"}
  {:id 17
@@ -122,6 +139,7 @@
   :type :site
   :map {:center {:latitude 44.940447 :longitude -93.166282} :zoom 14}
   :phone "tel:+1-651-225-8989"
+  :address "38 Snelling Ave S, St Paul, MN 55105"
   :storeLink "http://www.commongoodbooks.com"
   :storeName "Common Good Books"}
  {:id 18
@@ -129,6 +147,7 @@
   :type :site
   :map {:center {:latitude 44.940247 :longitude -93.137250} :zoom 14}
   :phone "tel:+1-651-224-8320"
+  :address "891 Grand Ave, St Paul, MN 55105"
   :storeLink "http://www.redballoonbookshop.com"
   :storeName "Red Balloon Bookshop"}
  {:id 19
@@ -136,6 +155,7 @@
   :type :site
   :map {:center {:latitude 45.057563 :longitude -92.805685} :zoom 14}
   :phone "tel:+1-651-430-3385"
+  :address "217 Main St N, Stillwater, MN 55082"
   :storeLink "http://www.valleybookseller.com"
   :storeName "Valley Bookseller"}
  {:id 20
@@ -143,6 +163,7 @@
   :type :site
   :map {:center {:latitude 44.924730 :longitude -93.313411} :zoom 14}
   :phone "tel:+1-612-920-5005"
+  :address "2720 W 43rd St, Minneapolis, MN 55410"
   :storeLink "http://www.wildrumpusbooks.com"
   :storeName "Wild Rumpus"}
  {:id 21
@@ -150,6 +171,7 @@
   :type :site
   :map {:center {:latitude 45.467730 :longitude -122.713105} :zoom 14}
   :phone "tel:+1-503-246-0053"
+  :address "7834 SW Capitol Hwy, Portland, OR 97219"
   :storeLink "http://www.annieblooms.com"
   :storeName "Annie Bloom's Books"}
  {:id 22
@@ -157,6 +179,7 @@
   :type :site
   :map {:center {:latitude 45.535182 :longitude -122.648269} :zoom 14}
   :phone "tel:+1-503-284-1726"
+  :address "1714 NE Broadway St, Portland, OR 97232"
   :storeLink "http://www.broadwaybooks.net"
   :storeName "Broadway Books"}
  {:id 23
@@ -164,6 +187,7 @@
   :type :powells
   :map {:center {:latitude 45.523377 :longitude -122.681319} :zoom 14}
   :phone "tel:+1-503-228-4651"
+  :address "1005 W Burnside St, Portland, OR 97209"
   :storeLink "http://www.powells.com/"
   :storeName "Powell's Books"}
  {:id 24
@@ -171,6 +195,7 @@
   :type :site
   :map {:center {:latitude 47.614661 :longitude -122.319602} :zoom 14}
   :phone "tel:+1-206-624-6600"
+  :address "1521 10th Ave, Seattle, WA 98122"
   :storeLink "http://www.elliottbaybook.com"
   :storeName "The Elliott Bay Book Company"}
  {:id 25
@@ -178,6 +203,7 @@
   :type :site
   :map {:center {:latitude 47.261673 :longitude -122.445302} :zoom 14}
   :phone "tel:+1-253-272-8801"
+  :address "218 St Helens Ave, Tacoma, WA 98402"
   :storeLink "http://www.kingsbookstore.com"
   :storeName "King's Books"}
 {:id 26
@@ -185,6 +211,7 @@
   :type :site
   :map {:center {:latitude 47.602697 :longitude -122.332991} :zoom 14}
   :phone "tel:+1-206-587-5737"
+  :address "117 Cherry St, Seattle, WA 98104"
   :storeLink "http://www.seattlemystery.com"
   :storeName "Seattle Mystery Bookshop"}
 {:id 27
@@ -192,6 +219,7 @@
   :type :solr
   :map {:center {:latitude 47.668888 :longitude -122.384664} :zoom 14}
   :phone "tel:+1-206-789-5006"
+  :address "2214 NW Market St, Seattle, WA 98107"
   :storeLink "http://www.secretgardenbooks.com"
   :storeName "The Secret Garden Bookshop"}
 {:id 28
@@ -199,5 +227,6 @@
   :type :site
   :map {:center {:latitude 47.753232 :longitude -122.280014} :zoom 14}
   :phone "tel:+1-206-366-3333"
+  :address "6504 20th Ave NE, Seattle, WA 98115"
   :storeLink "http://www.thirdplacebooks.com"
   :storeName "Third Place Books"}]
