3d8c95be462a534b81c559791b81594b5f81a4ba
Eric Weinstein <eric.q.weinstein@gmail.com>
2021-01-23 16:08:10 -0800

    Update project dependencies

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
 (defproject quixote "1.0.0"
   :description "Quixote: the search service for http://www.cityshelf.com/"
   :url "https://github.com/ericqweinstein/quixote"
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/math.numeric-tower "0.0.4"]
-                 [liberator "0.14.1"]
-                 [compojure "1.5.0"]
+                 [liberator "0.15.2"]
+                 [compojure "1.6.2"]
                  [cheshire "5.6.1"]
-                 [clj-http "3.1.0"]
-                 [ring/ring-core "1.5.0"]
-                 [ring/ring-jetty-adapter "1.5.0"]
+                 [clj-http "3.11.0"]
+                 [ring/ring-core "1.8.2"]
+                 [ring/ring-jetty-adapter "1.8.2"]
                  [ring-cors "0.1.8"]
                  [enlive "1.1.6"]
                  [hiccup "1.0.5"]]
